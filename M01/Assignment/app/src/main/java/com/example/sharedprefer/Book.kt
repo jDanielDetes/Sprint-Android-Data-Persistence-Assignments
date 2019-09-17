87df616764899c76ed367295778dd8c11c9a340c
jDanielDetes <jdanieldetes@gmail.com>
2019-09-17 17:30:22 -0400

    part 3

--- M01/Assignment/app/src/main/java/com/example/sharedprefer/Book.kt
+++ M01/Assignment/app/src/main/java/com/example/sharedprefer/Book.kt
@@ -1,8 +1,32 @@
 package com.example.sharedprefer
 
-class Book (
-    val title : String,
-    val reasonToRead: String,
-    val hasBeenRead : Boolean,
-    val id : String
-)
+class Book {
+    var title: String? = null
+    var reasonToRead: String? = null
+    var hasBeenRead: Boolean = false
+    var id: String? = null
+
+    constructor(title: String, reasonToRead: String, hasBeenRead: Boolean, id: String) {
+        this.title = title
+        this.reasonToRead = reasonToRead
+        this.hasBeenRead = hasBeenRead
+        this.id = id
+    }
+
+    constructor(csvString: String) {
+        val values = csvString.split(",")
+
+        if (values.size == 4) {
+            this.title = values[0]
+            this.reasonToRead = values[1].replace("~@", ",")
+            this.hasBeenRead = values[2].toBoolean()
+            this.id = values[3]
+        }
+    }
+
+    fun toCsvString(): String {
+        return "$title,${reasonToRead?.replace(",", "~@")},$hasBeenRead,$id"
+    }
+
+
+}
